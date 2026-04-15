dae1cc029517b7e13a3f3af916496f968cee3ca5
Rajat Maurya <rajatmaurya176@gmail.com>
2026-04-15 11:24:59 +0530

    Fix nested git repository issue to track files correctly

---
+++ csr/app/api/route.ts
@@ -0,0 +1,11 @@
+
+export async function GET() {
+  return Response.json({
+    success: true,
+    message: "Postify  Backend API is running throught Next.js 🚀",
+    endpoints: {
+      health: "/api/health",
+      docs: "API only backend"
+    }
+  });
+}
